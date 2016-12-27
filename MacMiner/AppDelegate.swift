4f975a52a05c4743fe617cdcff8eba87519e8824
fabulouspanda <john@fabulouspanda.co.uk>
2016-12-27 14:46:50 +0000

    Reattached menu bar
    To allow quitting the app

--- MacMiner/AppDelegate.swift
+++ MacMiner/AppDelegate.swift
@@ -11,7 +11,12 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    @IBOutlet weak var window:NSWindow!
+    @IBOutlet weak var menuBar:NSMenu!
     
+    @IBAction func quitSwiftMiner(sender:NSMenuItem) {
+    NSApplication.shared().terminate(self)
+    }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -19,6 +24,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        
     }
     
     
